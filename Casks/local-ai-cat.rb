454276442cb008a9a1bbee21597c2f2d394fd2d9
atlascodesai <76924051+atlascodesai@users.noreply.github.com>
2026-04-09 18:56:40 +0100

    bump: v1.2.18

--- Casks/local-ai-cat.rb
+++ Casks/local-ai-cat.rb
@@ -1,6 +1,6 @@
 cask "local-ai-cat" do
-  version "1.2.17"
-  sha256 "d2a791840099e4363d6067fb32d0a6645d0631a63150f084312449c3b931eef8"
+  version "1.2.18"
+  sha256 "3585f5da03eb5a8867b1007079c6a11768e1e68deb35d1eec8f723596f59a53c"
 
   url "https://github.com/local-ai-cat/localaicat-site/releases/download/v#{version}/LocalAIChatDirect-#{version}.dmg"
   name "Local AI Cat"
